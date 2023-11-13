654877613d04fe5b25a7b17fbed7c2865d848a3a
pierresouza <pierre.s3@hotmail.com>
2023-11-13 10:52:10 -0300

    fix test

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,5 +1,5 @@
 it("testa a página da política de privacidade de forma independente", function () {
   cy.visit("./src/privacy.html");
 
-  cy.contains("Talking About Tsting").should("be.visible");
+  cy.contains("Talking About Testing").should("be.visible");
 });
